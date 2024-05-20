b10d11bcc851a439fb34c9b56d875f1f6d67eb82
fpipio <francesco.pipio@gmail.com>
2024-05-20 09:35:26 +0300

    v0.3.2

--- src/modules/styles.js
+++ src/modules/styles.js
@@ -7,21 +7,21 @@ export const styles = `
     flex-wrap: wrap;
 }
 
-
 form {
     display: table;
 }
+
 .row {
     display: table-row;
 }
+
 .label, .value {
     display: table-cell;
     padding: 0.5em;
 }
 
-.media-type{
+.media-type {
     margin-bottom: 20px;
-
 }
 
 .artist-container {
@@ -36,31 +36,28 @@ form {
     white-space: nowrap;
 }
 
-.artist-detail-header{
+.artist-detail-header, .album-detail-header {
     display: flex;
     margin-bottom: 20px;
 }
 
-.artist-detail-header-info{
+.artist-detail-header-info, .album-detail-header-info {
     margin-left: 20px;
 }
 
-.album-detail-header{
-    display: flex;
-    margin-bottom: 20px;
-}
-
-.album-detail-header-info{
-    margin-left: 20px;
+.artist-name, .album-name {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 10px;
 }
 
 .artist-name {
     overflow: hidden;
-    text-overflow: ellipsis; /* Aggiunge i tre puntini se il testo eccede la larghezza del contenitore */
+    text-overflow: ellipsis;
     color: var(--primary-text-color);
 }
 
-.artist-image {
+.artist-image, .album-image {
     max-width: 145px;
     max-height: 145px;
     object-fit: cover;
@@ -68,48 +65,41 @@ form {
 }
 
 .search-input {
-    width: 100%; /* Imposta la larghezza al 100% del contenitore genitore */
-    padding: 8px; /* Aggiungi spazio intorno al testo all'interno dell'input */
-    border: 1px solid #ccc; /* Aggiungi un bordo sottile */
-    border-radius: 4px; /* Aggiungi angoli arrotondati */
-    box-sizing: border-box; /* Garantisce che il padding e il bordo siano inclusi nella larghezza totale */
+    width: 100%;
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
     margin-bottom: 5px;
 }
 
-/* Stile per la griglia degli album */
 .album-grid {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Griglia con colonne di larghezza minima di 200px */
-    grid-gap: 20px; /* Spaziatura tra le celle della griglia */
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    grid-gap: 20px;
 }
 
-/* Stile per ciascun elemento dell'album nella griglia */
 .album-item {
-    border: 1px solid #ccc; /* Bordo grigio */
-    padding: 10px; /* Spaziatura interna */
+    border: 1px solid #ccc;
+    padding: 10px;
 }
 
-/* Stile per l'immagine dell'album */
 .album-image {
-    width: 100%; /* Occupa tutta la larghezza dell'elemento */
-    height: auto; /* Altezza automatica */
+    width: 100%;
+    height: auto;
     object-fit: cover;
     aspect-ratio: 1 / 1;
 }
 
-/* Stile per il titolo dell'album */
 .album-title {
-    font-weight: bold; /* Testo in grassetto */
-    margin-top: 5px; /* Margine superiore */
+    font-weight: bold;
+    margin-top: 5px;
 }
 
-/* Stile per l'anno dell'album */
 .album-year {
-    color: #888; /* Colore grigio scuro */
-    font-size: 0.9em; /* Dimensione del carattere leggermente più piccola */
+    color: #888;
+    font-size: 0.9em;
 }
-
-
 
 .album-detail {
     padding: 20px;
@@ -118,74 +108,46 @@ form {
     background-color: #f9f9f9;
 }
 
-.album-image {
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    margin-bottom: 10px;
-    aspect-ratio: 1 / 1;
-
-}
-
-.album-name {
-    font-size: 18px;
-    font-weight: bold;
-    margin-bottom: 10px;
-}
-
 .track-container {
     margin-top: 20px;
 }
 
 .track-item {
+    display: flex;
+    align-items: center;
     padding: 10px;
     border-bottom: 1px solid #ccc;
+    margin-bottom: 5px;
 }
 
 .track-item:last-child {
-    border-bottom: none; /* Rimuove la linea inferiore dall'ultimo elemento */
+    border-bottom: none;
 }
 
 .track-item:hover {
-    background-color: #f0f0f0; /* Cambia il colore di sfondo al passaggio del mouse */
+    background-color: #f0f0f0;
 }
 
 .track-duration {
-    float: right; /* Posiziona la durata della traccia a destra */
+    float: right;
     font-size: 14px;
     color: #666;
-    margin-left: auto; 
-}
-
-.artist-detail .artist-name {
-    margin-bottom: 20px;
-    font-weight: bold; 
-    font-size: 18px;
-}
-
-
-.track-item {
-    display: flex; /* Utilizza il layout flexbox */
-    align-items: center; /* Allinea verticalmente al centro */
+    margin-left: auto;
 }
 
 .triangle-icon,
 .track-name {
-    margin-right: 10px; /* Aggiunge spazio tra gli elementi */
+    margin-right: 10px;
 }
-
-
 
 .playlist-container {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(145px, 1fr));
     grid-gap: 10px;
-
 }
 
 .playlist-item {
     border: 1px solid #ccc;
-    padding: 10px;
     cursor: pointer;
     transition: transform 0.2s ease;
 }
@@ -204,7 +166,6 @@ form {
     color: #666;
 }
 
-/* Stili per l'intestazione della playlist */
 .playlist-header {
     display: flex;
     align-items: center;
@@ -212,13 +173,11 @@ form {
 }
 
 .playlist-image {
-    width: 145px; /* Imposta la larghezza desiderata */
-    height: auto; /* Imposta l'altezza automaticamente in base all'immagine */
+    width: 145px;
+    height: auto;
     margin-right: 10px;
     object-fit: cover;
     aspect-ratio: 1 / 1;
-
-
 }
 
 .playlist-title {
@@ -227,19 +186,7 @@ form {
 }
 
 .total-duration {
-    margin-left: auto; /* Allinea la durata a destra */
-}
-
-/* Stili per le singole tracce */
-.track-item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-}
-
-.triangle-icon {
-    margin-right: 10px;
-    cursor: pointer; /* Cambia il cursore al passaggio sopra l'icona */
+    margin-left: auto;
 }
 
 .track-details {
@@ -253,11 +200,9 @@ form {
 }
 
 .author-album {
-    font-style: italic; /* Aggiunge stile corsivo all'autore */
+    font-style: italic;
     padding-right: 5px;
 }
-
-
 
 .playlist-type {
     font-size: 14px;
@@ -278,5 +223,6 @@ form {
 .play-button:hover {
     background-color: #0056b3;
 }
+
 
 `;
